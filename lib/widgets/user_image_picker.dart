3b7415fcf3739aed893ef835310121e0aba29a1d
brianphchen <brian770130@gmail.com>
2024-08-27 14:15:26 -0700

    feat: add upload image

--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -4,9 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
-  const UserImagePicker({super.key});
+  const UserImagePicker({
+    super.key,
+    required this.onPickImage,
+  });
 
-  // final void Function(File pickedImage) onPickImage;
+  final void Function(File pickedImage) onPickImage;
 
   @override
   State<UserImagePicker> createState() {
@@ -32,7 +35,7 @@ class _UserImagePickerState extends State<UserImagePicker> {
       _pickedImageFile = File(pickedImage.path);
     });
 
-    // widget.onPickImage(_pickedImageFile!);
+    widget.onPickImage(_pickedImageFile!);
   }
 
   @override
